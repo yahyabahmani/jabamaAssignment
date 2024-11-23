0ab6af6fe91cc9820023a40c9057bbabb0d58272
shadi.ghelman <shadi.ghelman@snapp.cab>
2024-11-24 01:05:00 +0330

    feat: add a simple vertical list view

--- FiveSquare/Views/ListedPlacesMap.swift
+++ FiveSquare/Views/ListedPlacesMap.swift
@@ -10,17 +10,18 @@ import SwiftUI
 
 struct ListedPlacesMap: View {
     let manager = PlaceManager()
+    @State private var isPresentingList = false
     @State private var selectedPlace: Model.ID?
     @State private var searchText = ""
     @FocusState private var isSearchFocused
-    
+
     func searchBar() -> some View {
         HStack {
             Image(systemName: "magnifyingglass")
-            
+
             TextField("Search", text: $searchText)
                 .focused($isSearchFocused)
-            
+
             Button("Cancel", systemImage: "xmark.circle.fill") {
                 searchText = ""
                 isSearchFocused = false
@@ -33,9 +34,37 @@ struct ListedPlacesMap: View {
         .background(.background, in: .capsule)
         .onTapGesture { isSearchFocused = true }
         .onSubmit(of: .text) { manager.onSearchSubmit(text: searchText) }
-        
         .padding(.horizontal)
         .animation(.spring, value: isSearchFocused)
+    }
+
+    private func orientedPlacesList(_ axes: Axis.Set = .horizontal) -> PlacesList {
+        PlacesList(
+            axes: axes,
+            places: manager.places.map { .init($0) },
+            scrollPosition: $selectedPlace,
+            onLastAppear: manager.onLastAppear,
+            isLoading: manager.isLoading
+        )
+    }
+
+    private func adoptedPlacesList() -> some View {
+        orientedPlacesList()
+            .safeAreaInset(edge: .top) {
+                Button("List Mode", systemImage: "list.bullet") {
+                    isPresentingList.toggle()
+                }
+                .controlSize(.large)
+                .bold()
+                .buttonStyle(.borderedProminent)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .labelStyle(.iconOnly)
+                .padding(.horizontal)
+                .tint(Color(.systemBackground))
+                .foregroundStyle(.primary)
+            }
+            .padding(.top)
+            .transition(.move(edge: .bottom).combined(with: .opacity))
     }
 
     var body: some View {
@@ -47,19 +76,18 @@ struct ListedPlacesMap: View {
         .ignoresSafeArea(.keyboard)
         .overlay(alignment: .bottom) {
             if !manager.places.isEmpty {
-                PlacesList(
-                    axes: .horizontal,
-                    places: manager.places.map { .init($0) },
-                    scrollPosition: $selectedPlace,
-                    onLastAppear: manager.onLastAppear,
-                    isLoading: manager.isLoading
-                )
-                .transition(.move(edge: .bottom).combined(with: .opacity))
+                adoptedPlacesList()
             }
         }
         .safeAreaInset(edge: .top, content: searchBar)
         .animation(.spring, value: selectedPlace)
         .animation(.spring, value: manager.places.isEmpty)
+        .sheet(isPresented: $isPresentingList) {
+            orientedPlacesList(.vertical)
+                .presentationDragIndicator(.visible)
+                // Make some room for the presentation drag indicator
+                .padding(.top)
+        }
     }
 }
 
