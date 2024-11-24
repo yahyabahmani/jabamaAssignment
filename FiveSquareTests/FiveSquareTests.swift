e708678ec998c4c01a651514da7607f6fbb61b98
shadi.ghelman <shadi.ghelman@snapp.cab>
2024-11-25 00:34:23 +0330

    test: add a basic test for the on last appear method

--- FiveSquareTests/FiveSquareTests.swift
+++ FiveSquareTests/FiveSquareTests.swift
@@ -60,4 +60,51 @@ final class FiveSquareTests: XCTestCase {
         XCTAssertEqual(self.placesManager.places.first?.name, "Test-1")
         XCTAssertEqual(self.placesManager.nextURL, expectedNextUrl)
     }
+    
+    func testOnLastAppear() async throws {
+        // Given
+        let currentPlaces = [
+            Place(
+                id: "1",
+                geocodes: .init(main: .init(latitude: 12, longitude: 12)),
+                name: "Test-1",
+                distance: 12
+            ),
+            Place(
+                id: "2",
+                geocodes: .init(main: .init(latitude: 12, longitude: 12)),
+                name: "Test-2",
+                distance: 12
+            )
+        ]
+        let nextPagePlaces = [
+            Place(
+                id: "3",
+                geocodes: .init(main: .init(latitude: 12, longitude: 12)),
+                name: "Test-3",
+                distance: 12
+            )
+        ]
+        
+        let nextURL = URL(string: "Test")
+        let expectation = XCTestExpectation(description: "Call Get Places")
+        self.placesManager.places = currentPlaces
+        // Assuming it already has a next url pending
+        self.placesManager.nextURL = nextURL
+        self.placesManager.webservice.getPlacesNextPage = { _ in
+            expectation.fulfill()
+            // Simulating the last page response
+            return (nextPagePlaces, nil)
+        }
+        
+        // When
+        self.placesManager.onLastAppear()
+        
+        // Then
+        await fulfillment(of: [expectation], timeout: 1)
+        
+        XCTAssertEqual(self.placesManager.places.count, 3)
+        XCTAssertEqual(self.placesManager.places.last?.name, nextPagePlaces.last?.name)
+        XCTAssertEqual(self.placesManager.nextURL, nil)
+    }
 }
