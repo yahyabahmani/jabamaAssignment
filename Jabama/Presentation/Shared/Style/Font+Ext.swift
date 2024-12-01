e2543f51c76fc65154c94f531b074d92ca67b3a9
Mohsen <mr.marsxan@gmail.com>
2024-12-01 22:06:17 +0330

    add custom fonts

---
+++ Jabama/Presentation/Shared/Style/Font+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  Font+Ext.swift
+//  Xplorify
+//
+//  Created by Mohsen on 11/17/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static let pageTitle = Font.custom("Inter-Medium", size: 22)
+    static let chipsText = Font.custom("Inter-Regular", size: 14)
+    static let kBody = Font.custom("Inter-Regular", size: 14)
+    static let buttonText = Font.custom("Inter-Medium", size: 15)
+    static let navBarTitle = Font.custom("Inter-Medium", size: 11)
+    static let cardInfo = Font.custom("Inter-Regular", size: 14)
+    static let smallBody = Font.custom("Inter-Regular", size: 12)
+    static let buttonStrong = Font.custom("Inter-Bold", size: 15)
+    static let dialogTitle = Font.custom("Inter-Medium", size: 18)
+}
