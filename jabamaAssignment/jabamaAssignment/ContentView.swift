40588df53c970cd1b59fff8e2d5042cb9c175458
Amir <amir@Amirs-MacBook-Pro.local>
2024-12-04 11:55:51 +0330

    chore(project-setup): initial commit with Xcode project and test targets

---
+++ jabamaAssignment/jabamaAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jabamaAssignment
+//
+//  Created by Amir  on 04/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jabamaAssignment/jabamaAssignment/jabamaAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  jabamaAssignmentApp.swift
+//  jabamaAssignment
+//
+//  Created by Amir  on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct jabamaAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
