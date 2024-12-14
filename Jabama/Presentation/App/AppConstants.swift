a4154827e1a8b85ab258c314a41b403ad5c9fc14
Mohsen <mr.marsxan@gmail.com>
2024-12-14 16:57:11 +0330

    remove api keys

--- Jabama/Presentation/App/AppConstants.swift
+++ Jabama/Presentation/App/AppConstants.swift
@@ -10,10 +10,10 @@ import Foundation
 struct AppConstants {
 #if DEBUG
 static let BASE_URL = URL(string: "https://api.foursquare.com/v3/")!
-static let API_KEY = "fsq3w4QfcEVJPD+7CWBpIoJL9sDMQJLm3wcfuHKk0nJyli0="
+static let API_KEY = ""
 
 #else
 static let BASE_URL = URL(string: "https://api.foursquare.com/v3/")!
-static let API_KEY = "fsq3w4QfcEVJPD+7CWBpIoJL9sDMQJLm3wcfuHKk0nJyli0="
+static let API_KEY = ""
 #endif
 }
